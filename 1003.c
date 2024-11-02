c5ab7cd5486fad593451b6b189f19a3edc23ebeb
Cleitonbvalente <135654920+Cleitonbvalente@users.noreply.github.com>
2024-11-02 01:03:19 -0300

    1003.c

---
+++ 1003.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x, y, SOMA;
+    scanf("%d",&x);
+    scanf("%d", &y);
+    SOMA = x + y;
+    printf("SOMA = %d\n", SOMA);
+    
+}
