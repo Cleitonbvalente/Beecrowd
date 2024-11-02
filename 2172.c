54e37ff6c9c2e8347449ad466e70c16516963edc
Cleitonbvalente <135654920+Cleitonbvalente@users.noreply.github.com>
2024-11-02 00:41:41 -0300

    2172.c

---
+++ 2172.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    long long int x, y;
+    while(1)
+    {
+        scanf("%lld%lld", &x, &y);
+        if(x==0 && y==0) break;
+        printf("%lld\n", x*y);
+    }
+    return 0;
+}
